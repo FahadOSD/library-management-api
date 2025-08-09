1b61707be1f5a6b4c92d89e03a9bf860f503bb66
FahadOSD <fahad1000mir@email.com>
2025-08-09 07:23:22 +0600

    Register models in admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile, Author, Category, Book, Borrow
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Book)
+admin.site.register(Borrow)
